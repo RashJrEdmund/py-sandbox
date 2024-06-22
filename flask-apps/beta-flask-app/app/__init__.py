0c47dcf4bcc34aee230d087026bc9a58159abd2c
rash <orashusedmund@gmail.com>
2024-06-22 21:27:23 +0100

    installed sqlalchemy and initialized the user model and project.db file

--- flask-apps/beta-flask-app/app/__init__.py
+++ flask-apps/beta-flask-app/app/__init__.py
@@ -3,6 +3,8 @@
 
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy # checkout the sql_alchemy docs https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/
+
 from app.blueprints import ( # app.blueprints is referencing the blueprints folder
   index, # this is the index file
   users, # the users file
@@ -11,8 +13,29 @@ from app.blueprints import ( # app.blueprints is referencing the blueprints fold
 
 app = Flask(__name__)
 
+# DATA BASE CONFIGURATION using SQLAlchemy
+db = SQLAlchemy() # this initialization automatically creates the instance folder in app
+
+# configure the SQLite database, relative to the app instance folder
+# note that this uses a file in the same folder as app. "project.db".
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+# initialize the app with the extension
+db.init_app(app)
+
+#
+## registering blueprints
 app.register_blueprint(index.index_bp)
 
 app.register_blueprint(users.user_bp)
 
 app.register_blueprint(blogs.blog_bp)
+
+# Creating models
+class UserModel(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
+
+with app.app_context():
+  db.create_all() # to create all database models in the instance folder in app
--- flask-apps/beta-flask-app/app/blueprints/users.py
+++ flask-apps/beta-flask-app/app/blueprints/users.py
@@ -2,8 +2,14 @@
   This file has everything to do with the users route
 """
 from flask import (
-  Blueprint, request, render_template
+  Blueprint, render_template,
+  request, redirect,
+  url_for,
 )
+
+# from app import ( # app here is the app folder referencing the __init__ file
+#   UserModel, # use these docs to know how to query using db.session, https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/#query-the-data
+# )
 
 user_bp = Blueprint("users", __name__, url_prefix="/users")
 
@@ -11,6 +17,9 @@ template_dir = "users"
 
 @user_bp.route("/") # default methods +=>  methods=["GET"]
 def get_all_users():
+  # users = UserModel.query.all()
+  # print(users)
+
   return render_template(f"{template_dir}/all.html")
 
 @user_bp.route("/create/")
@@ -23,8 +32,23 @@ def user_route(user_name):
 
 @user_bp.route("/", methods=["POST"])
 def add_user():
+  #json = request.json # request content type should be application/json first before we can get the json
   method = request.method
-  # json = request.get_json(force=True)
-  # print("\n json", json, "\n")
+  form = request.form
+
+  # return form # sends back, as json, the form
+
+  # store user in db
+
+  return redirect(url_for("users.create_user_page"))
+  """
+    url_for("users.create_user_page") simple means;
+    "go to the user blueprint, find a function called 'create_user_page'
+    and return it's route". So in this case, it'll return "users/create"
+
+    Since submitting the form in the create_user_page() handler redirects to
+    this add_user() handler, I'm using the redirect() function along side the
+    url_for("users.create_user_page") to redirect back to the form page.
+  """
 
   return f"creating user: method: {method}, name: json.name"
